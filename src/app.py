86ed82eefc400c61956146dc90c2a50032649c16
Mammoth1930 <mamoth1930@gmail.com>
2023-08-04 16:52:53 +1000

    unable to build database bug fixed

--- src/app.py
+++ src/app.py
@@ -11,13 +11,12 @@ from database import db_init
 from api import update_dataset, tables_to_csv
 from dashboard import get_layout
 
-app = Dash(__name__)
-app.layout = get_layout
-
 if __name__ == '__main__':
 
     db_init()
     update_dataset()
     tables_to_csv()
 
+    app = Dash(__name__)
+    app.layout = get_layout
     app.run_server(debug=True)
